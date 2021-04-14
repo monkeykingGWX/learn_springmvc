ad0db72d02d2b398a91b8b5bfe9a27713749d53e
gwx <704835519@qq.com>
2021-04-15 00:19:28 +0800

    mavenpreview

---
+++ maven02/src/main/java/com/studymaven/model/User.java
@@ -0,0 +1,37 @@
+package com.studymaven.model;
+
+/**
+ * @author monkeykingGWX
+ * email 704835519@qq.com
+ * created 2021-04-15 0:06
+ */
+
+
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+}
---
+++ maven03/src/main/java/com/studymaven/model/Country.java
@@ -0,0 +1,28 @@
+package com.studymaven.model;
+
+/**
+ * @author monkeykingGWX
+ * email 704835519@qq.com
+ * created 2021-04-15 0:13
+ */
+
+
+public class Country {
+
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
